3301f853a57938e3cf15123c1a05344072972aeb
Foziljonov Muhsinjon <foziljonovmuhsinjon@gmail.com>
2024-12-26 14:47:51 +0500

    better user display created

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import React from 'react'
 import UserList from './components/UserList';
 import Users from "./assets/users.json"
+import appStyles from "./App.module.css"
 
 function App() {
 
     return (
-        <>
-        <h1>Users</h1>
-        <div className="wrapper">
-            {
-                Users.length > 0 && Users.map((user, index) => {
-                    return <UserList data={user} key={index} />
-                })
-            }
+      <>
+        <h1 className={appStyles.heading}>Users</h1>
+        <div className={appStyles.wrapper}>
+          {Users.length > 0 &&
+            Users.map((user, index) => {
+              return <UserList data={user} key={index} />;
+            })}
         </div>
-        </>
+      </>
     );
 }
 
